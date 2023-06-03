f77ec4c332c10b3377b812d9e49cb174e8bfea70
rahul singh <95570957+rahul-singh2021@users.noreply.github.com>
2023-06-03 01:22:46 -0700

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,18 +3,21 @@ import numpy as np
 def gradient_descent(x,y):
   w_curr=b_curr=0
   iterations=1000
-  alpha=0.001
+  alpha=0.08
   m=len(x)
 
   for i in range(iterations):
     y_prd=w_curr*x+b_curr
+    cost=(1/(2*m))*np.sum(val**2 for val in (y_prd-y) )
     wd=(1/(2*m))*np.sum(x*(y_prd-y))
     bd=(1/(2*m))*np.sum(y_prd-y)
     w_curr=w_curr-alpha*wd
     b_curr=b_curr-alpha*bd
-    print("w{},b{},iterations{}".format(w_curr,b_curr,i))
+    print("w{},b{},cost{},iterations{}".format(w_curr,b_curr,cost,i))
 
 x=np.array([1,2,3,4,5])
 y=np.array([5,7,9,11,13])
 
 gradient_descent(x,y)
+
+
